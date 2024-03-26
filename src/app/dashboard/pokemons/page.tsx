eb53b42ab0e5b2bc91ff4b9a76c97bffe5d22713
Gerbric <gerbric14@gmail.com>
2024-03-26 01:45:01 -0500

    Empezando el dashboard

---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,39 @@
+import { PokemonsReponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokemons = async (limit = 20, offset = 0):Promise<SimplePokemon[]> => {
+  const data: PokemonsReponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    .then(res => res.json());
+
+    const pokemons = data.results.map(pokemon => ({
+      id: pokemon.url.split('/').at(-2)!,
+      name: pokemon.name,
+    }))
+
+    return pokemons;
+}
+
+export default async function PokemonsPage() {
+
+  const pokemons = await getPokemons();
+  console.log(pokemons);
+  
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+
+        {pokemons.map(({id, name}) => (
+          <Image 
+            key={id}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+            alt={name}
+            width={100}
+            height={100}
+          />
+        ))}
+
+      </div>
+    </div>
+  )
+}
